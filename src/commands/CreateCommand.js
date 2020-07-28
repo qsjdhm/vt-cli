3b478ceff3d52600a5039a9f05d51cf9ac8bf673
qsjdhm <qsjdhm@163.com>
2020-07-28 20:22:07 +0800

    init

--- src/commands/CreateCommand.js
+++ src/commands/CreateCommand.js
@@ -16,109 +16,128 @@ const { InquirerConfig, RepoPath } = require('../utils/config');
  * @param {} destination 用户输入的create命令的参数
  */
 class Creator {
-  constructor(source, destination, ops = {}) {
-    this.source = source
-    this.cmdParams = parseCmdParams(destination)
-    this.RepoMaps = Object.assign({
-      repo: RepoPath,
-      temp: path.join(__dirname, '../../__temp__'),
-      target: ''
-    //   target: this.genTargetPath(this.source)
-    }, ops);
-    this.gitUser = {}
-    this.spinner = ora()
-    this.init()
-  }
+    constructor(source, destination, ops = {}) {
+        this.source = source
+        this.cmdParams = parseCmdParams(destination)
+        this.RepoMaps = Object.assign({
+            repo: RepoPath,
+            temp: path.join(__dirname, '../../__temp__'),
+            target: this.genTargetPath(this.source)
+        }, ops);
+        this.gitUser = {}
+        this.spinner = ora()
+        this.init()
+    }
 
-  // 生成目标文件夹的绝对路径
-  genTargetPath(relPath = 'vue-ts-template') {
-    return path.resolve(process.cwd(), relPath);
-  }
+    // 生成目标文件夹的绝对路径
+    genTargetPath(relPath = 'vue-ts-template') {
+        return path.resolve(process.cwd(), relPath);
+    }
 
   
 
-  // 初始化函数
-  async init() {
-    try {
-        await this.downloadRepo()
+    // 初始化函数
+    async init() {
+        try {
+            await this.inputProjectGit()
+            await this.copyRepoFiles();
+            // // 监测文件夹是否存在
+            // await this.checkFolderExist();
+            // // 用户选择分支并复制到目标项目目录下
+            // await this.getFrameBranchs();
+            // // 让用户输入目标项目的git地址，用于生成git信息
+            // await this.inputProjectGit()
+            
 
-        // 让用户输入目标项目的文件名
-        await this.inputProjectName()
-        // 监测文件夹是否存在
-        await this.checkFolderExist();
-        // 用户选择分支并复制到目标项目目录下
-        await this.getFrameBranchs();
+           
         //   await this.copyRepoFiles();
-    //   await this.downloadRepo();
-    //   await this.copyRepoFiles();
-    //   await this.initGit();
-    //   await this.updatePkgFile();
-    //   await this.initGit();
-    //   await this.runApp();
-    } catch (error) {
-      console.log('')
-      log.error(error);
-      exit(1)
-    } finally {
-      this.spinner.stop();
-    }
-  }
-
-  // 让用户输入目标项目的文件名
-  async inputProjectName () {
-    const { inputNewName } = await inquirer.prompt(InquirerConfig.rename);
-    this.RepoMaps.target = this.genTargetPath(inputNewName)
-    console.info(this.RepoMaps.target)
-  }
-
-  // 监测文件夹是否存在
-  checkFolderExist() {
-    return new Promise(async (resolve, reject) => {
-      const { target } = this.RepoMaps
-      // 如果create附加了--force或-f参数，则直接执行覆盖操作
-      if (this.cmdParams.force) {
-        await fs.removeSync(target)
-        return resolve()
-      }
-      try {
-        // 否则进行文件夹检查
-        const isTarget = await fs.pathExistsSync(target)
-        if (!isTarget) return resolve()
-
-        const { recover } = await inquirer.prompt(InquirerConfig.folderExist);
-        if (recover === 'cover') {
-          await fs.removeSync(target);
-          return resolve();
-        } else if (recover === 'newFolder') {
-          const { inputNewName } = await inquirer.prompt(InquirerConfig.rename);
-          this.source = inputNewName;
-          this.RepoMaps.target = this.genTargetPath(`./${inputNewName}`);
-          return resolve();
-        } else {
-          exit(1);
+        //   await this.downloadRepo();
+        //   await this.copyRepoFiles();
+        //   await this.initGit();
+        //   await this.updatePkgFile();
+        //   await this.initGit();
+        //   await this.runApp();
+        } catch (error) {
+            console.log('')
+            log.error(error);
+            exit(1)
+        } finally {
+            this.spinner.stop();
         }
-      } catch (error) {
-        log.error(`[vta]Error:${error}`)
-        exit(1);
-      }
-    })
-  }
+    }
+
+    // 监测文件夹是否存在
+    checkFolderExist() {
+        return new Promise(async (resolve, reject) => {
+            const { target } = this.RepoMaps
+            // 如果create附加了--force或-f参数，则直接执行覆盖操作
+            if (this.cmdParams.force) {
+                await fs.removeSync(target)
+                return resolve()
+            }
+            try {
+                // 否则进行文件夹检查
+                const isTarget = await fs.pathExistsSync(target)
+                if (!isTarget) return resolve()
+
+                const { recover } = await inquirer.prompt(InquirerConfig.folderExist);
+                if (recover === 'cover') {
+                    await fs.removeSync(target);
+                    return resolve();
+                } else if (recover === 'newFolder') {
+                    const { inputNewName } = await inquirer.prompt(InquirerConfig.rename);
+                    this.source = inputNewName;
+                    this.RepoMaps.target = this.genTargetPath(`./${inputNewName}`);
+                    return resolve();
+                } else {
+                    exit(1);
+                }
+            } catch (error) {
+                log.error(`[vta]Error:${error}`)
+                exit(1);
+            }
+        })
+    }
 
     // 获取框架代码，用户选择分支并复制到目标项目目录下
     async getFrameBranchs () {
-        this.spinner.start('正在拉取项目模板源码...');
+        let frameGitAddress = '';
+        const { frameType } = await inquirer.prompt(InquirerConfig.frameList);
+        if (frameType === 'pc') {
+            frameGitAddress = 'https://github.com/qsjdhm/zymulinput.git'
+        } else if (frameType === 'client') {
+            frameGitAddress = 'github:qsjdhm/zymulinput'
+        } else if (frameType === 'hybrid') {
+            frameGitAddress = 'github:qsjdhm/zymulinput'
+        }
+        this.spinner.start('正在获取框架版本信息...');
         const { repo, temp } = this.RepoMaps
         return new Promise(async (resolve, reject) => {
             await fs.removeSync(temp);
-            await runCmd(`git clone ${this.source} ${temp}`)
-            this.spinner.succeed('模版下载成功');
+            // 将框架代码clone到__temp__目录下，通过.git目录获取框架代码分支版本信息
+            await runCmd(`git clone ${frameGitAddress} ${temp}`)
             try {
-                await runCmd(`cd ${this.RepoMaps.temp}`)
-                process.chdir(this.RepoMaps.temp);
+                await runCmd(`cd ${temp}`)
+                process.chdir(temp);
                 const list = await runCmd('git branch -a')
-                console.info(111)
-                console.info(list.split('\n'))
-                this.spinner.succeed('获取分支成功');
+                this.spinner.succeed('获取框架版本信息成功');
+                // 过滤出有用的分支
+                const usefulBranches = this.filterUsefulBranches(list.split('\n'))
+                InquirerConfig.frameBranchs.choices = usefulBranches
+                const { frameBranch } = await inquirer.prompt([{
+                    type: 'list',
+                    name: 'frameBranch',
+                    message: '请选择框架分支版本：',
+                    choices: usefulBranches
+                }]);
+                await runCmd(`cd ../`)
+                process.chdir('../');
+                // clone选中分支的代码
+                await fs.removeSync(temp);
+                // 将框架代码clone到__temp__目录下，通过.git目录获取框架代码分支版本信息
+                this.spinner.start('正在获取'+frameBranch+'版本框架源码...');
+                await runCmd(`git clone -b ${frameBranch} ${frameGitAddress} ${temp}`)
+                this.spinner.succeed(frameBranch+'版本框架源码下载成功');
             } catch (error) {
                 reject(error)
             } finally {
@@ -127,6 +146,78 @@ class Creator {
         })
     }
 
+    // 过滤出有用的分支
+    filterUsefulBranches (list) {
+        // 过滤无用的分支信息
+        let branchList = []
+        list.forEach(item => {
+            let branchTemp = item.split('remotes/origin/')
+            if (branchTemp.length > 1 && branchTemp[1].indexOf('HEAD ->') === -1) {
+                branchList.push({name: branchTemp[1], value: branchTemp[1]})
+            }
+        })
+        return branchList
+    }
+
+    // 让用户输入目标项目的git地址，用于生成git信息
+    async inputProjectGit () {
+        const { inputGitAddress } = await inquirer.prompt({
+            name: 'inputGitAddress',
+            type: 'input',
+            message: '请输入项目的GIT地址: '
+        });
+        // 创建目录，并克隆项目
+        await fs.mkdir(this.RepoMaps.target)
+        console.info(12344)
+        await runCmd(`git clone ${inputGitAddress} ${this.RepoMaps.target}`)
+        
+        // await fs.removeSync(temp);
+        // this.spinner.start('正在克隆项目代码...');
+        // await runCmd(`git clone -b master ${inputGitAddress} ${temp}`)
+        // this.spinner.succeed(frameBranch+'版本框架源码下载成功');
+
+
+
+        // await runCmd(`git clone -b ${frameBranch} ${frameGitAddress} ${temp}`)
+        // https://github.com/qsjdhm/test.git
+        // console.info(this.RepoMaps.target)
+        // this.RepoMaps.target = this.genTargetPath(inputGitAddress)
+    }
+
+    // 拷贝repo资源
+    async copyRepoFiles() {
+        const { temp, target } = this.RepoMaps
+        await copyFiles(temp, target, ['./git', './changelogs']);
+        await runCmd(`git add . `)
+        await runCmd(`git commit -m "init"`)
+        await runCmd(`git remote add origin ${inputGitAddress}`)
+        await runCmd(`git push -u origin master`)
+    }
+
+
+    // 下载repo资源
+    downloadRepo () {
+        this.spinner.start('正在拉取项目模板...');
+        const { repo, temp } = this.RepoMaps
+        return new Promise(async (resolve, reject) => {
+            await fs.removeSync(temp);
+            download(repo, temp, async err => {
+                if (err) return reject(err);
+                this.spinner.succeed('模版下载成功');
+                return resolve()
+            })
+        })
+    }
+
+    // 让用户输入目标项目的git地址，用于生成git信息
+    async initGit() {
+        const { inputGitAddress } = await inquirer.prompt(InquirerConfig.gitAddress);
+        this.spinner.start('正在初始化Git管理项目...');
+        await runCmd(`cd ${this.RepoMaps.target}`);
+        process.chdir(this.RepoMaps.target);
+        await runCmd(`git init`);
+        this.spinner.succeed('Git初始化完成！');
+    }
 
   // 下载repo资源
   downloadRepo() {
@@ -157,11 +248,6 @@ class Creator {
   }
 
 
-  // 拷贝repo资源
-  async copyRepoFiles() {
-    const { temp, target } = this.RepoMaps
-    await copyFiles(temp, target, ['./git', './changelogs']);
-  }
 
   // 更新package.json文件
   async updatePkgFile() {
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,21 +1,45 @@
 exports.InquirerConfig = {
-  // 文件夹已存在的名称的询问参数
-  folderExist: [{
-    type: 'list',
-    name: 'recover',
-    message: '当前文件夹已存在，请选择操作：',
-    choices: [
-      { name: '创建一个新的文件夹', value: 'newFolder' },
-      { name: '覆盖', value: 'cover' },
-      { name: '退出', value: 'exit' },
-    ]
-  }],
-  // 重命名的询问参数
-  rename: [{
-    name: 'inputNewName',
-    type: 'input',
-    message: '请输入新的项目名称: '
-  }]
+    // 文件夹已存在的名称的询问参数
+    folderExist: [{
+        type: 'list',
+        name: 'recover',
+        message: '当前文件夹已存在，请选择操作：',
+        choices: [
+            { name: '创建一个新的文件夹', value: 'newFolder' },
+            { name: '覆盖', value: 'cover' },
+            { name: '退出', value: 'exit' },
+        ]
+    }],
+    // 重命名的询问参数
+    rename: [{
+        name: 'inputNewName',
+        type: 'input',
+        message: '请输入新的项目名称: '
+    }],
+    // 文件夹已存在的名称的询问参数
+    frameList: [{
+        type: 'list',
+        name: 'frameType',
+        message: '请选择框架类型：',
+        choices: [
+            { name: 'PC端', value: 'pc' },
+            { name: '现场客户端', value: 'client' },
+            { name: '移动端', value: 'hybrid' },
+        ]
+    }],
+    // 框架分支列表
+    frameBranchs: [{
+        type: 'list',
+        name: 'frameBranch',
+        message: '请选择框架分支版本：',
+        choices: []
+    }],
+    // 目标项目的git地址询问参数
+    gitAddress: [{
+        name: 'inputGitAddress',
+        type: 'input',
+        message: '请输入项目的GIT地址: '
+    }],
 }
 
 // 远程Repo地址
